e5853e171d486477e4d7f95e1ecedb25f12a3206
Blackstar <85343553+YongJ7@users.noreply.github.com>
2022-01-09 13:35:11 +0900

    Update 왕위 계승.cpp

--- 1월6일-HASH/[백준] 5021-왕위 계승/용제/왕위 계승.cpp
+++ 1월6일-HASH/[백준] 5021-왕위 계승/용제/왕위 계승.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <unordered_map>
 using namespace std;
+unordered_map<string, double> arr;
+string fam_tree[51][3];
+
+int N, M;
+string king, s, ans;
+double mm;
+
 int main()
 {
 	cin.tie(0);
 	cin.sync_with_stdio(0);
 
-	unordered_map<string, int> arr;
-	int N, M, cnt = 0;
-	cin >> N >> M;
-	//scanf("%d,%d", &N, &M);
-	
+	cin >> N >> M >> king;
+	arr[king] = 1.0;
+
 	for (int i = 0; i < N; i++) {
-		string s;
-		cin >> s;
-		arr[s] = 0;
+		for (int j = 0; j < N; j++) {
+			if(i == 0) cin >> fam_tree[j][0] >> fam_tree[j][1] >> fam_tree[j][2];
+			arr[fam_tree[j][0]] = (arr[fam_tree[j][2]] + arr[fam_tree[j][1]]) / 2;
+		}
 	}
 
-	for (int i = 0; i < M; i++) {
-		string s;
+	for (int i = 0; i < M; i++)
+	{
 		cin >> s;
-		if (arr.find(s) != arr.end()) cnt++;
+		if (arr.find(s) != arr.end() && arr[s] > mm)
+		{
+			ans = s;
+			mm = arr[s];
+		}
 	}
-	cout << cnt;
-	//printf("%d", cnt);
+
+	cout << ans;
 }
