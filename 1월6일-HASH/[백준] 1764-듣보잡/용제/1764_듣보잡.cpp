df10c23fa43c2d76ced59fea5b91fffa57af6afb
Blackstar <85343553+YongJ7@users.noreply.github.com>
2022-01-09 13:36:12 +0900

    Update 1764_듣보잡.cpp

--- 1월6일-HASH/[백준] 1764-듣보잡/용제/1764_듣보잡.cpp
+++ 1월6일-HASH/[백준] 1764-듣보잡/용제/1764_듣보잡.cpp
@@ -1,27 +1,26 @@
-#include <iostream>
-#include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
+int n, m, cnt;
+unordered_map<string, int> p;
+vector<string> v;
 int main()
 {
-	cin.tie(0);
-	cin.sync_with_stdio(0);
-
-	unordered_map<string, int> arr;
-	int N, M, cnt = 0;
-	cin >> N >> M;
-	//scanf("%d,%d", &N, &M);
-	
-	for (int i = 0; i < N; i++) {
-		string s;
-		cin >> s;
-		arr[s] = 0;
-	}
-
-	for (int i = 0; i < M; i++) {
-		string s;
-		cin >> s;
-		if (arr.find(s) != arr.end()) cnt++;
-	}
-	cout << cnt;
-	//printf("%d", cnt);
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cin >> n >> m;
+    for (int i = 0; i < n + m; i++)
+    {
+        string s;
+        cin >> s;
+        p[s]++;
+        if (p[s] > 1)
+        {
+            v.push_back(s);
+            cnt++;
+        }
+    }
+    sort(v.begin(),v.end());
+    cout << cnt << '\n';
+    for (int i = 0; i < cnt; i++)
+        cout << v[i] << '\n';
 }
